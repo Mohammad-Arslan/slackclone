c3781344f9df18c7f8c0bd3945f7b1b0b43d6151
arslaniks <shanitoor73@gmail.com>
2025-01-29 00:36:40 +0500

    workspace work completed

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,6 +2,14 @@ import { v } from "convex/values"; // Importing value validation functions
 import { mutation, query } from "./_generated/server"; // Importing mutation and query functions for server interactions
 import { auth } from "./auth"; // Importing authentication functions
 
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+  return code;
+};
+
 // The 'create' mutation allows users to create a new workspace
 export const create = mutation({
   args: {
@@ -18,13 +26,19 @@ export const create = mutation({
       throw new Error("Unauthorized");
     }
 
-    const joinCode = "12345"; // A static join code for the workspace
+    const joinCode = generateCode();
 
     // Insert a new workspace into the database with the provided name, user ID, and join code
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode,
+    });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
     });
     // Return the ID of the newly created workspace
     return workspaceId;
@@ -35,8 +49,29 @@ export const create = mutation({
 export const get = query({
   args: {}, // No arguments are required for this query
   handler: async (ctx) => {
-    // Query the database for all workspaces and collect the results
-    return await ctx.db.query("workspaces").collect();
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+    // Query t he database for all workspaces and collect the results
+    return workspaces;
   },
 });
 
@@ -49,6 +84,29 @@ export const getById = query({
       throw new Error("unauthorized");
     }
 
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!members) {
+      return null;
+    }
+
     return await ctx.db.get(args.id);
   },
+  // handler: async (ctx, args) => {
+  //   // New method using ctx.auth
+  //   const identity = await ctx.auth.getUserIdentity();
+
+  //   if (!identity) {
+  //     throw new Error("Unauthorized");
+  //   }
+  //   // The user ID is available in identity.subject
+  //   const userId = identity.subject;
+
+  //   return await ctx.db.get(args.id);
+  // },
 });
--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,13 +1,23 @@
-import { UserButton } from "@/features/auth/components/user-button"
-import { WorkspaceSwitcher } from "./workspace-switcher"
+"use client";
+
+import { UserButton } from "@/features/auth/components/user-button";
+import { WorkspaceSwitcher } from "./workspace-switcher";
+import { SidebarButton } from "./sidebar-button";
+import { Bell, Home, MessagesSquare, MoreHorizontal } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export const Sidebar = () => {
-    return (
-        <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
-            <WorkspaceSwitcher/>
-            <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
-                <UserButton/>
-            </div>
-        </aside>
-    )
-}
+    const pathName =usePathname();
+  return (
+    <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
+      <WorkspaceSwitcher />
+      <SidebarButton icon={Home} label="Home" isActive={pathName.includes("/workspace")} />
+      <SidebarButton icon={MessagesSquare} label="DMs" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
+      <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+        <UserButton />
+      </div>
+    </aside>
+  );
+};
